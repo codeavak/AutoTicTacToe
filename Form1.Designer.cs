a40c9412815ef687dc71b2a3a6143f25d1cb51ed
Johnny Avakian <javak@yahoo.com>
2018-03-21 15:26:52 -0700

    VIP

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl00 = new System.Windows.Forms.Label();
+            this.lbl01 = new System.Windows.Forms.Label();
+            this.lbl02 = new System.Windows.Forms.Label();
+            this.lbl12 = new System.Windows.Forms.Label();
+            this.lbl11 = new System.Windows.Forms.Label();
+            this.lbl10 = new System.Windows.Forms.Label();
+            this.lbl22 = new System.Windows.Forms.Label();
+            this.lbl21 = new System.Windows.Forms.Label();
+            this.lbl20 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl00
+            // 
+            this.lbl00.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl00.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl00.Location = new System.Drawing.Point(12, 9);
+            this.lbl00.Name = "lbl00";
+            this.lbl00.Size = new System.Drawing.Size(80, 80);
+            this.lbl00.TabIndex = 0;
+            this.lbl00.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl01
+            // 
+            this.lbl01.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl01.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl01.Location = new System.Drawing.Point(110, 9);
+            this.lbl01.Name = "lbl01";
+            this.lbl01.Size = new System.Drawing.Size(80, 80);
+            this.lbl01.TabIndex = 1;
+            this.lbl01.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl02
+            // 
+            this.lbl02.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl02.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl02.Location = new System.Drawing.Point(206, 9);
+            this.lbl02.Name = "lbl02";
+            this.lbl02.Size = new System.Drawing.Size(80, 80);
+            this.lbl02.TabIndex = 2;
+            this.lbl02.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl12
+            // 
+            this.lbl12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl12.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl12.Location = new System.Drawing.Point(206, 103);
+            this.lbl12.Name = "lbl12";
+            this.lbl12.Size = new System.Drawing.Size(80, 80);
+            this.lbl12.TabIndex = 5;
+            this.lbl12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl11
+            // 
+            this.lbl11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl11.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl11.Location = new System.Drawing.Point(110, 103);
+            this.lbl11.Name = "lbl11";
+            this.lbl11.Size = new System.Drawing.Size(80, 80);
+            this.lbl11.TabIndex = 4;
+            this.lbl11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl10
+            // 
+            this.lbl10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl10.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl10.Location = new System.Drawing.Point(12, 103);
+            this.lbl10.Name = "lbl10";
+            this.lbl10.Size = new System.Drawing.Size(80, 80);
+            this.lbl10.TabIndex = 3;
+            this.lbl10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl22
+            // 
+            this.lbl22.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl22.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl22.Location = new System.Drawing.Point(206, 194);
+            this.lbl22.Name = "lbl22";
+            this.lbl22.Size = new System.Drawing.Size(80, 80);
+            this.lbl22.TabIndex = 8;
+            this.lbl22.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl21
+            // 
+            this.lbl21.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl21.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl21.Location = new System.Drawing.Point(110, 194);
+            this.lbl21.Name = "lbl21";
+            this.lbl21.Size = new System.Drawing.Size(80, 80);
+            this.lbl21.TabIndex = 7;
+            this.lbl21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl20
+            // 
+            this.lbl20.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl20.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl20.Location = new System.Drawing.Point(12, 194);
+            this.lbl20.Name = "lbl20";
+            this.lbl20.Size = new System.Drawing.Size(80, 80);
+            this.lbl20.TabIndex = 6;
+            this.lbl20.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 313);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(274, 22);
+            this.txtResult.TabIndex = 9;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(110, 367);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 10;
+            this.btnPlay.Text = "Play!";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 408);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lbl22);
+            this.Controls.Add(this.lbl21);
+            this.Controls.Add(this.lbl20);
+            this.Controls.Add(this.lbl12);
+            this.Controls.Add(this.lbl11);
+            this.Controls.Add(this.lbl10);
+            this.Controls.Add(this.lbl02);
+            this.Controls.Add(this.lbl01);
+            this.Controls.Add(this.lbl00);
+            this.Name = "Form1";
+            this.Text = "Auto Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl00;
+        private System.Windows.Forms.Label lbl01;
+        private System.Windows.Forms.Label lbl02;
+        private System.Windows.Forms.Label lbl12;
+        private System.Windows.Forms.Label lbl11;
+        private System.Windows.Forms.Label lbl10;
+        private System.Windows.Forms.Label lbl22;
+        private System.Windows.Forms.Label lbl21;
+        private System.Windows.Forms.Label lbl20;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnPlay;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        string[,] gameBoard;
+        List<string> takenBoard;
+        Random rnd;
+        int moveCounter;
+        string whosTurn;
+
+        void Initialize()
+        {
+            takenBoard = new List<string>();
+            foreach (Control c in this.Controls)
+            {
+                if (c is Label)
+                {
+                    c.Text = "";
+                }
+            }
+            txtResult.Clear();
+            rnd = new Random();
+            gameBoard = new string[3, 3];
+            moveCounter = 1;
+            whosTurn = rnd.Next(0, 2) == 1 ? "X" : "O";
+
+            while (checkWin() == 0 && moveCounter < 10)
+            {
+                play();
+                moveCounter++;
+            }
+            int result = checkWin();
+            DrawBoard();
+            if (checkWin() == 1)
+                txtResult.Text = "Player 1 won!";
+            else if (result == 2)
+                txtResult.Text = "Player 2 won!";
+            else txtResult.Text = "We have a Tie!";
+
+        }
+        void play()
+        {
+            int row, column;
+            row = rnd.Next(0, 3);
+            column = rnd.Next(0, 3);
+            while (takenBoard.Contains($"{row}{column}"))
+            {
+                row = rnd.Next(0, 3);
+                column = rnd.Next(0, 3);
+            }
+            takenBoard.Add($"{row}{column}");
+            gameBoard[row, column] = whosTurn;
+            SwitchTurn();
+
+        }
+        void DrawBoard()
+        {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    string LabelText = $"{i}{j}";
+                    foreach (Control c in this.Controls)
+                    {
+
+                        if (c is Label && c != null)
+                        {
+                            if (c.Name.EndsWith(LabelText))
+                                c.Text = gameBoard[i, j];
+                        }
+                    }
+                }
+
+        }
+        int checkWin()
+        {
+            if (gameBoard[0, 0] == "X" && gameBoard[0, 1] == "X" && gameBoard[0, 2] == "X" ||
+
+             gameBoard[1, 0] == "X" && gameBoard[1, 1] == "X" && gameBoard[1, 2] == "X" ||
+             gameBoard[2, 0] == "X" && gameBoard[2, 1] == "X" && gameBoard[2, 2] == "X" ||
+             gameBoard[0, 0] == "X" && gameBoard[1, 0] == "X" && gameBoard[2, 0] == "X" ||
+             gameBoard[0, 1] == "X" && gameBoard[1, 1] == "X" && gameBoard[2, 1] == "X" ||
+             gameBoard[0, 2] == "X" && gameBoard[1, 2] == "X" && gameBoard[2, 2] == "X" ||
+             gameBoard[0, 0] == "X" && gameBoard[1, 1] == "X" && gameBoard[2, 2] == "X" ||
+
+             gameBoard[0, 2] == "X" && gameBoard[1, 1] == "X" && gameBoard[2, 0] == "X")
+            {
+                return 1;
+            }
+            else if (gameBoard[0, 0] == "O" && gameBoard[0, 1] == "O" && gameBoard[0, 2] == "O" ||
+
+           gameBoard[1, 0] == "O" && gameBoard[1, 1] == "O" && gameBoard[1, 2] == "O" ||
+           gameBoard[2, 0] == "O" && gameBoard[2, 1] == "O" && gameBoard[2, 2] == "O" ||
+           gameBoard[0, 0] == "O" && gameBoard[1, 0] == "O" && gameBoard[2, 0] == "O" ||
+           gameBoard[0, 1] == "O" && gameBoard[1, 1] == "O" && gameBoard[2, 1] == "O" ||
+           gameBoard[0, 2] == "O" && gameBoard[1, 2] == "O" && gameBoard[2, 2] == "O" ||
+           gameBoard[0, 0] == "O" && gameBoard[1, 1] == "O" && gameBoard[2, 2] == "O" ||
+
+           gameBoard[0, 2] == "O" && gameBoard[1, 1] == "O" && gameBoard[2, 0] == "O")
+
+                return 2;
+            else return 0;
+
+        }
+
+        void SwitchTurn()
+        {
+            if (whosTurn == "X")
+                whosTurn = "O";
+            else
+                whosTurn = "X";
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            Initialize();
+
+        }
+    }
+}
